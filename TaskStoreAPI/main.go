158cd13211269f718e02e47773293b5215e72143
Vadim <vadimx30@gmail.com>
2023-10-31 11:27:37 +0300

    Finished TaskHandler

--- TaskStoreAPI/main.go
+++ TaskStoreAPI/main.go
@@ -16,11 +16,13 @@ https://github.com/golang-standards/project-layout/blob/master/README_ru.md
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"TaskStoreAPI/internal/taskstore"
 )
@@ -35,15 +37,42 @@ func NewTaskServer() *taskServer {
 }
 
 func (ts *taskServer) taskHandler(w http.ResponseWriter, r *http.Request) {
+	//Request is only '/task/' URL without ID
 	if r.URL.Path == "/task/" {
 		if r.Method == http.MethodPost {
 			ts.createTaskHandler(w, r)
-		} else if r.Method == http.MethodGet {
-			ts.getTaskByIdHandler(w, r)
+		} else if r.Method == http.MethodGet{
+			ts.getAllTaskHandler(w, r)
+		} else if r.Method == http.MethodDelete{
+			ts.deleteAllTaskHandler(w, r)
+		} else {
+			http.Error(w, fmt.Sprint("expect method GET, POST, DELETE at '/task', got %v", r.Method), http.StatusMethodNotAllowed)
+			return
+		}
+		
+	} else {
+		// Request has an ID as '/task/<id>' URL
+		path := strings.Trim(r.URL.Path, "/")
+		pathParts := strings.Split(path, "/")
+		if len(pathParts) < 2{
+			http.Error(w, fmt.Sprint("expect 'task/<id>' in task handler"), http.StatusBadRequest)
+			return
+		}
+		id, err := strconv.Atoi(pathParts[1])
+		if err != nil{
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		if r.Method == http.MethodGet {
+			ts.getTaskHandler(w, r, int(id))
+		} else if r.Method == http.MethodDelete{
+			ts.deleteTaskHandler(w, r, int(id))
+		} else {
+			http.Error(w, fmt.Sprint("expect method GET, DELETE at '/task<id>', got %v", r.Method), http.StatusMethodNotAllowed)
+			return
 		}
 	}
 }
-
 func main() {
 	mux := http.NewServeMux()
 	server := NewTaskServer()
